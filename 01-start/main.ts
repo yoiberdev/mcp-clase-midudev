27bbc539b5ae410c7dc584782f7efc4fc183c88e
Yoiber dev <yoiber696@gmail.com>
2025-04-18 23:26:31 -0500

    first commit

---
+++ 01-start/main.ts
@@ -0,0 +1,30 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+const server = new McpServer({
+  name: "chaika",
+  version: "0.0.1",
+});
+
+server.tool(
+  "fetch-weather",
+  "Tool to fetch the weather of a city",
+  {
+    city: z.string().describe("City name"),
+  },
+  async ({ city }) => {
+    return {
+      content: [
+        {
+          type: "text",
+          text: `El clima de ${city} es soleado`,
+        },
+      ],
+    };
+  }
+);
+
+
+const transport = new StdioServerTransport()
+await server.connect(transport)
